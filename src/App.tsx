c4e3cbd0021a797246bf71f255d12e94a62ca222
Saravjeet Singh <saravjeetsingh.malik@gmail.com>
2022-10-08 21:15:46 -0400

    initial content dummy created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './content/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h3>Hello World</h3>
-    </div>
+    <BrowserRouter>
+      <Navbar/>
+    
+      {/* Client side navigation */}
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <>
+
+        <footer className="bg-white bottom-0 absolute w-full rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+            <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+            </span>
+        </footer>
+
+        </>
+    )
+}
+
+export default Footer
